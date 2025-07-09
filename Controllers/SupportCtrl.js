9464ab8bbf03b3a6bfba941932352dfaa63c1d1b
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-07-09 13:49:14 +0530

    d

--- Controllers/SupportCtrl.js
+++ Controllers/SupportCtrl.js
@@ -2,15 +2,29 @@ import Contact from "../Models/SupportModel.js";
 import asyncHandler from "express-async-handler";
 
 export const createContact = asyncHandler(async (req, res) => {
-  const { name, email, subject, message } = req.body;
+  const { customerId, subject, message } = req.body;
 
-  const newContact = await Contact.create({ name, email, subject, message });
+  const newContact = await Contact.create({ customerId, subject, message });
   res.status(201).json({ message: "Contact saved", data: newContact });
 });
 
 export const getAllContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
-  res.status(200).json({ data: contacts });
+  const contacts = await Contact.find().populate('customerId');
+
+  const formatted = contacts.map((item) => ({
+    customerId: item.customerId?._id,
+    _id: item?._id,
+    customerName: item.customerId?.customerName,
+    companyName: item.customerId?.companyName,
+    email: item.customerId?.email,
+    phoneNumber: item.customerId?.phoneNumber,
+    subject: item.subject,
+    message: item.message,
+    createdAt: item.createdAt,
+    updatedAt: item.updatedAt,
+  }));
+
+  res.status(200).json({ data: formatted });
 });
 
 export const deleteContact = asyncHandler(async (req, res) => {
--- Models/SupportModel.js
+++ Models/SupportModel.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    subject: { type: String },
+customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Customer",
+  },    subject: { type: String },
     message: { type: String },
 }, {
     timestamps: true
